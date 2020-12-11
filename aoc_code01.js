b0eabd99054fd6d3c3be9d72b4f20e3333c12c23
Marcelo <marcelo.gittermann@gmail.com>
2020-12-11 23:07:39 +0100

    first puzzle solved

---
+++ aoc_code01.js
@@ -0,0 +1,24 @@
+// const fs = require("fs");
+
+// fs.open('data01.txt', 'r', (err, fd) => {
+  // if (err) throw err;
+
+  // console.log("hej", fd);
+
+  // fs.close(fd, (err) => {
+    // if (err) throw err;
+  // });
+// });
+
+const data = require("./data.json")
+
+
+function main(list) {
+	var item = list.pop();
+	
+	
+	
+}
+
+
+looper(data);
---
+++ exc1/exc1.js
@@ -0,0 +1,70 @@
+var fs = require('fs');
+
+var filename = 'data1.txt';
+
+var hej = fs.readFile(filename, 'utf8', function(err, data) {
+
+  if (err) throw err;
+
+  console.log('OK: ' + filename);
+
+ 
+
+  const content = data;
+
+  processFile(content);
+
+});
+
+ 
+
+function processFile(content) {
+
+   
+
+ var dataArray = content.split("\n").map(element => parseInt(element));
+
+ var answerProductA;
+
+ var answerProductB;
+
+ 
+
+ while (dataArray.length>1) {
+
+	  var  productA = dataArray.pop();
+
+	 
+
+	  var counter = 0;
+
+	  dataArray.forEach(function(productB) {
+
+	   counter++;
+
+	   
+	   if(productA + productB === 2020) {
+
+			answerProductA = productA;
+
+			answerProductB = productB;
+			
+			console.log(answerProductA*answerProductB, "is the answer");
+
+	   }
+
+								  
+
+								  
+
+	  });
+
+							  //console.log("length: "+(dataArray.length+1)+"; innerloops: "+counter);
+
+ }
+
+                            
+
+                            
+
+}
